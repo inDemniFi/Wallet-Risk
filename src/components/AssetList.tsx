eb23081fd2212d529c5679dc36056131537cfe8d
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-09 22:03:37 -0500

    Update AssetList.tsx

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -1,13 +1,45 @@
-// AssetList.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAppStore } from "../AppStore";
 
 const AssetList: React.FC = () => {
-  // Implement the component logic to fetch and display asset data
+  const { fireblocksNCW } = useAppStore();
+  const [assets, setAssets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSupportedAssets = async () => {
+      try {
+        const supportedAssets = await fireblocksNCW.getSupportedAssets();
+        setAssets(supportedAssets);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching supported assets:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchSupportedAssets();
+  }, [fireblocksNCW]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      {/* Display the list of assets */}
+      <h2>Supported Assets</h2>
+      <ul>
+        {assets.map((asset) => (
+          <li key={asset.assetId}>
+            <strong>Name:</strong> {asset.name}
+            <br />
+            <strong>Symbol:</strong> {asset.symbol}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default AssetList;
+
