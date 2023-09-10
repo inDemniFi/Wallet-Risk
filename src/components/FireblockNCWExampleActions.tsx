58ef89cc5072306ed05b10355e86085b78a3dd34
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-09 21:59:09 -0500

    Update FireblockNCWExampleActions.tsx

--- src/components/FireblockNCWExampleActions.tsx
+++ src/components/FireblockNCWExampleActions.tsx
@@ -5,8 +5,8 @@ import { GenerateMPCKeys } from "./GenerateMPCKeys";
 import { Takeover } from "./Takeover";
 import { Transactions } from "./Transactions";
 import { Web3 } from "./Web3";
-import { AssetList } from "./AssetList";
-import { AssetBalance } from "./AssetBalance";
+import AssetList from "./AssetList";
+import AssetBalance from "./AssetBalance";
 
 export const FireblockNCWExampleActions: React.FC = () => {
   return (
