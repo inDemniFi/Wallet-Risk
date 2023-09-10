6fb23c65b54dc52cfdbf7f6183b696c2cf7fa261
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-09 21:16:20 -0500

    Create AssetList.tsx

---
+++ src/components/AssetList.tsx
@@ -0,0 +1,13 @@
+// AssetList.tsx
+import React from "react";
+
+const AssetList: React.FC = () => {
+  // Implement the component logic to fetch and display asset data
+  return (
+    <div>
+      {/* Display the list of assets */}
+    </div>
+  );
+};
+
+export default AssetList;
