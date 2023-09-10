a21409ea2188d5ee55bc243a24f0396be43004d2
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-09 21:16:46 -0500

    Create AssetBalance.tsx

---
+++ src/components/AssetBalance.tsx
@@ -0,0 +1,13 @@
+// AssetBalance.tsx
+import React from "react";
+
+const AssetBalance: React.FC = () => {
+  // Implement the component logic to fetch and display asset balance
+  return (
+    <div>
+      {/* Display the asset balance */}
+    </div>
+  );
+};
+
+export default AssetBalance;
