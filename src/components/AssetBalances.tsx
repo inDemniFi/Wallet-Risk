70d6c7d7ef71657b21f48766849939b4df2e870f
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-10 10:50:01 -0500

    Create AssetBalances.tsx

---
+++ src/components/AssetBalances.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { ApiService, ITransactionDetails } from "./ApiService"; // Import your ApiService and ITransactionDetails
+
+interface AssetBalancesProps {
+  deviceId: string;
+  accountId: string;
+}
+
+const AssetBalances: React.FC<AssetBalancesProps> = ({ deviceId, accountId }) => {
+  const [assetBalances, setAssetBalances] = useState<{ [assetId: string]: number }>({});
+
+  useEffect(() => {
+    const apiService = new ApiService("your_base_url", "your_token"); // Replace with your actual base URL and token
+    const fetchTransactions = async () => {
+      try {
+        const response = await apiService.getTransactions(deviceId, accountId); // Replace with the actual API call to fetch transactions
+        const transactions: ITransactionDetails[] = await response.json();
+
+        // Calculate the netAmount for each asset and update the balances
+        const balances: { [assetId: string]: number } = {};
+        transactions.forEach((transaction) => {
+          const { assetId, netAmount } = transaction;
+          if (assetId && netAmount !== undefined) {
+            balances[assetId] = (balances[assetId] || 0) + netAmount;
+          }
+        });
+
+        setAssetBalances(balances);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      }
+    };
+
+    fetchTransactions();
+  }, [deviceId, accountId]);
+
+  return (
+    <div>
+      <h2>Asset Balances</h2>
+      <ul>
+        {Object.entries(assetBalances).map(([assetId, balance]) => (
+          <li key={assetId}>
+            {assetId}: {balance} {assetId}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AssetBalances;
