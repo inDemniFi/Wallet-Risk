3ac0cc3ca8ae9b6ca2c7e9cfcd1d69d39037b87f
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-10 11:12:19 -0500

    Update and rename assetBalancesConfig.tsx to AssetBalancesConfig.tsx

---
+++ src/components/AssetBalancesConfig.tsx
@@ -0,0 +1,13 @@
+export const assetBalancesConfig = [
+  {
+    id: '1',
+    assetName: 'Bitcoin',
+    balance: 10.5,
+  },
+  {
+    id: '2',
+    assetName: 'Ethereum',
+    balance: 100.0,
+  },
+  // Add more assets as needed
+];
